b09e080fd1a997c35ff5968c130759289dea2766
DataMas <yannismslr@gmail.com>
2024-04-17 15:33:19 +0300

    Add tests for the category controller

--- src/test/java/com/cern/todo/TodoApplicationTests.java
+++ src/test/java/com/cern/todo/TodoApplicationTests.java
@@ -1,24 +1,32 @@
 package com.cern.todo;
 
 import com.cern.todo.Controller.TaskCategoryController;
+import com.cern.todo.DTOs.TaskUpdateDTO;
+import com.cern.todo.Entities.Task;
 import com.cern.todo.Entities.TaskCategory;
 import com.cern.todo.Repository.TaskCategoryRepository;
+import com.cern.todo.Services.TaskService;
+import com.cern.todo.Utils.ApiResponse;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
-import java.util.Set;
+import java.util.*;
 
 @ExtendWith(MockitoExtension.class)
 class TodoApplicationTests {
@@ -27,6 +35,8 @@ class TodoApplicationTests {
 	private TaskCategoryRepository taskCategoryRepository;
 	@InjectMocks
 	private TaskCategoryController taskCategoryController;
+	@Mock
+	private TaskService taskService;
 
 	private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
@@ -36,19 +46,89 @@ class TodoApplicationTests {
 	}
 
 	@Test
+	public void testGetAllCategories() {
+
+		List<TaskCategory> categories = new ArrayList<>();
+		categories.add(new TaskCategory());
+		when(taskCategoryRepository.findAll()).thenReturn(categories);
+
+		List<TaskCategory> result = taskCategoryController.getAllCategories();
+
+		assertEquals(categories.size(), result.size());
+	}
+
+	@Test
 	public void testCreateCategory() {
-
+		// Mocking
 		TaskCategory category = new TaskCategory();
-		category.setCategoryId(1L);
-		category.setCategoryName("Test Category");
-
 		when(taskCategoryRepository.save(any(TaskCategory.class))).thenReturn(category);
 
-		ResponseEntity<?> response = taskCategoryController.createCategory(category);
+		// Test
+		ResponseEntity<?> response = taskCategoryController.createCategory(new TaskCategory());
 
+		// Assertion
 		assertEquals(HttpStatus.CREATED, response.getStatusCode());
-		assertEquals(category, response.getBody());
+		assertFalse(response.getBody() == null);
 	}
+
+	@Test
+	public void testGetCategoryById_ExistingId() {
+		// Mocking
+		Long id = 1L;
+		TaskCategory category = new TaskCategory();
+		category.setCategoryId(id);
+		when(taskCategoryRepository.findById(id)).thenReturn(Optional.of(category));
+
+		// Test
+		ResponseEntity<TaskCategory> response = taskCategoryController.getCategoryById(id);
+
+		// Assertion
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertFalse(response.getBody() == null);
+	}
+
+	@Test
+	public void testUpdateCategory() {
+		Long id = 1L;
+		TaskCategory category = new TaskCategory();
+		category.setCategoryId(id);
+		when(taskCategoryRepository.findById(id)).thenReturn(Optional.of(category));
+		when(taskCategoryRepository.save(any(TaskCategory.class))).thenReturn(category);
+
+		ResponseEntity<?> response = taskCategoryController.updateCategory(id, new TaskCategory());
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertFalse(response.getBody() == null);
+	}
+
+	@Test
+	public void testDeleteCategory() {
+		Long id = 1L;
+		TaskCategory category = new TaskCategory();
+		category.setCategoryId(id);
+		when(taskCategoryRepository.findById(id)).thenReturn(Optional.of(category));
+
+		ResponseEntity<?> response = taskCategoryController.deleteCategory(id);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
+	@Test
+	public void testCreateTask() {
+		// Mocking
+		Long categoryId = 1L;
+		TaskUpdateDTO taskRequest = new TaskUpdateDTO();
+		Task task = new Task();
+		when(taskService.createTask(any(TaskUpdateDTO.class), anyLong())).thenReturn(task);
+
+		// Test
+		ResponseEntity<?> response = taskCategoryController.createTask(categoryId, taskRequest);
+
+		// Assertion
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertFalse(response.getBody() == null);
+	}
+
 
 	@Test
 	public void testCreateCategory_ValidationException() {
